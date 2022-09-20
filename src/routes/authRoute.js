40465727e1fc7e60bc6476eac1ab8981d6525a73
pisutthee <pisut.flyinggiraffe@gmail.com>
2022-09-20 15:02:36 +0700

    feature login

--- src/routes/authRoute.js
+++ src/routes/authRoute.js
@@ -5,5 +5,6 @@ const authController = require('../controllers/authController');
 const router = express.Router();
 
 router.post('/register', authController.register);
+router.post('/login', authController.login);
 
 module.exports = router;
